494fde3c7d47d310fc76bbabc5f4a57cb10fd8e6
Higor Jugler <higorjugler@hotmail.com>
2022-08-11 08:43:53 -0300

    Implementado class Startup

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Startup.cs
@@ -0,0 +1,33 @@
+﻿namespace StudentAdminPortal.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        // WebApplication = IApplicationBuilder
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
